0fb56b2fdb4447629141626c1284d32395b284aa
l1sha1 <92680300+l1sha1@users.noreply.github.com>
2021-10-19 13:29:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+from typing import List
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+triangle_matrix: List[List[float]] = [[-2, -1, 1, -2], [0, 3, -3, 0], [0, 0, 4, 4]]
+var = triangle_matrix[2][len(triangle_matrix[2]) - 1] / triangle_matrix[2][len(triangle_matrix[2]) - 2]
+print(var)
+var_1 = (triangle_matrix[1][len(triangle_matrix[1]) - 1] - var * triangle_matrix[1][len(triangle_matrix[1]) - 2]) / \
+        triangle_matrix[1][len(triangle_matrix[1]) - 3]
+print(var_1)
+vars = []
+string_1 = []
+print(vars)
+vars.append(var)
+for i in range(len(triangle_matrix[1]) - 2, 0, -1):
+    triangle_matrix[1][i] = triangle_matrix[1][i] * vars[0]
+print(triangle_matrix[1])
+for i in range(len(triangle_matrix[1]) - 1):
+    if triangle_matrix[1][i] != 0:
+        string_1.append(triangle_matrix[1][i])
+else:
+    string_1.append(triangle_matrix[1][len(triangle_matrix[1]) - 1])
+    string_1[1] = string_1[2] - string_1[1]
+    del string_1[2]
+var = string_1[1] / string_1[0]
+vars.append(var)
+print(vars)
+print(string_1)
