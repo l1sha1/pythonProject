f0c296ec9e0589b9bc80b48e7dff58846d2dc6d1
l1sha1 <92680300+l1sha1@users.noreply.github.com>
2021-10-22 00:33:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
-from typing import List
-
-triangle_matrix: List[List[float]] = [[-2, -1, 1, -2], [0, 3, -3, 0], [0, 0, 4, 4]]
-var = triangle_matrix[2][len(triangle_matrix[2]) - 1] / triangle_matrix[2][len(triangle_matrix[2]) - 2]
-print(var)
-var_1 = (triangle_matrix[1][len(triangle_matrix[1]) - 1] - var * triangle_matrix[1][len(triangle_matrix[1]) - 2]) / \
-        triangle_matrix[1][len(triangle_matrix[1]) - 3]
-print(var_1)
-vars = []
-string_1 = []
+vars = [1 for i in range(len(matrix))]
+for i in range(len(matrix) - 1, -1, -1):
+    for k in range(len(matrix[i]) - 1):
+        matrix[i][k] *= vars[k]
+    for k in range(len(matrix[i]) - 2, i, -1):
+        if i == len(matrix) - 1:
+            break
+        else:
+            matrix[i][-1] -= matrix[i][k]
+    vars[i] = matrix[i][-1] / matrix[i][i]
 print(vars)
-vars.append(var)
-for i in range(len(triangle_matrix[1]) - 2, 0, -1):
-    triangle_matrix[1][i] = triangle_matrix[1][i] * vars[0]
-print(triangle_matrix[1])
-for i in range(len(triangle_matrix[1]) - 1):
-    if triangle_matrix[1][i] != 0:
-        string_1.append(triangle_matrix[1][i])
-else:
-    string_1.append(triangle_matrix[1][len(triangle_matrix[1]) - 1])
-    string_1[1] = string_1[2] - string_1[1]
-    del string_1[2]
-var = string_1[1] / string_1[0]
-vars.append(var)
-print(vars)
-print(string_1)
