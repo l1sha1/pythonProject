76803b741567f5934b40488fe38a1982d50e177e
l1sha1 <92680300+l1sha1@users.noreply.github.com>
2021-10-22 17:32:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
+from typing import List
+
+matrix: List[List[float]] = [[1, 2, -4, 3], [2, -3, 3, -1], [3, 2, -2, 5]] # Задание матрицы - списки в списке с типом данных float
+# Построение треугольной матрицы
+counter = 0
+while counter != len(matrix) - 1: # количество циклов должно быть больше размерности матрицы - 1
+    print(counter, "номер цикла")
+    """Цикл пробегает по строкам, делая первый ненулевой коэффициент равным 1""""
+    for i in range(counter, len(matrix)):
+        if matrix[i][counter] != 0:
+            p = matrix[i][counter]
+        elif matrix[i][counter] == 1:
+            p = matrix[i][counter]
+        else:
+            p = matrix[i][counter + 1]
+        for j in range(len(matrix[i])):
+            matrix[i][j] = matrix[i][j] / p
+        print(i)
+        print(*matrix, sep='\n')
+    """Цикл пробегает по строкам, записывая в строку значения после вычитания из этой строки предыдущей"""
+    for i in range(len(matrix) - 1, counter, -1):
+        for k in range(len(matrix[i])):
+            matrix[i][k] = matrix[i][k] - matrix[i - 1][k]
+    counter += 1
+    print()
+    print(*matrix, sep='\n')
+# Расчет корней
+"""Задаем вектор неизвестных, состоящий из числа 1, равного количеству неизвестных"""
 vars = [1 for i in range(len(matrix))]
+""""""
 for i in range(len(matrix) - 1, -1, -1):
     for k in range(len(matrix[i]) - 1):
         matrix[i][k] *= vars[k]
@@ -7,5 +36,5 @@ for i in range(len(matrix) - 1, -1, -1):
             break
         else:
             matrix[i][-1] -= matrix[i][k]
-    vars[i] = matrix[i][-1] / matrix[i][i]
+    vars[i] = round(matrix[i][-1] / matrix[i][i])
 print(vars)
