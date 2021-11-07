eb7bf31d38459e610e4af9ab5be0ab0f97d687b0
l1sha1 <92680300+l1sha1@users.noreply.github.com>
2021-11-07 21:44:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+# Задание матрицы - списки в списке с типом данных float
 import sys
 
 
 def gauss(matrix):
-# Построение треугольной матрицы
+    # Построение треугольной матрицы
     counter = 0
-    while counter != len(matrix) - 1: # количество циклов должно быть больше размерности матрицы - 1
+    while counter != len(matrix) - 1:  # количество циклов должно быть больше размерности матрицы - 1
         print(counter, "номер цикла")
+        matrix = sorted(matrix, key=lambda matrix: abs(matrix[counter]), reverse=True)
+        print(matrix)
         """Цикл пробегает по строкам, делая первый ненулевой коэффициент равным 1"""
         for i in range(counter, len(matrix)):
             if matrix[i][counter] != 0:
@@ -14,11 +17,27 @@ def gauss(matrix):
                 p = matrix[i][counter]
             else:
                 p = matrix[i][counter + 1]
-            for j in range(len(matrix[i])):
-                matrix[i][j] = matrix[i][j] / p
+            """Решение ситуации, когда уже в начале два или больше первых коэффициентов равны 0"""
+            try:
+                for j in range(len(matrix[i])):
+                    matrix[i][j] = matrix[i][j] / p
+            except ZeroDivisionError:
+                zeros = 0
+                for j in range(len(matrix[i])):
+                    if matrix[i][j] == 0:
+                        zeros += 1
+                    else:
+                        break
+                if zeros == len(matrix[i]) - 2:
+                    p = matrix[i][-2]
+                else:
+                    p = matrix[i][len(matrix[i]) - zeros]
+                for j in range(len(matrix[i])):
+                    matrix[i][j] = matrix[i][j] / p
+
             print(i)
             print(*matrix, sep='\n')
-        """Цикл пробегает по строкам, записывая в строку значения после вычитания из этой строки предыдущей"""
+            """Цикл пробегает по строкам, записывая в строку значения после вычитания из этой строки предыдущей"""
         for i in range(len(matrix) - 1, counter, -1):
             if matrix[i][counter] == 0:
                 continue
@@ -28,7 +47,7 @@ def gauss(matrix):
         counter += 1
         print()
         print(*matrix, sep='\n')
-"""Проверка, есть ли корни у системы"""
+
     for g in range(len(matrix)):
         counter_null = 0
         for j in range(len(matrix[g]) - 1):
@@ -41,17 +60,20 @@ def gauss(matrix):
             sys.exit()
     # Расчет корней
     """Задаем вектор неизвестных, состоящий из числа 1, равного количеству неизвестных"""
-    vars = [1 for i in range(len(matrix))]
-    for i in range(len(matrix) - 1, -1, -1):
-        for k in range(len(matrix[i]) - 1):
-            matrix[i][k] *= vars[k]
-        for k in range(len(matrix[i]) - 2, i, -1):
-            if i == len(matrix) - 1:
-                break
-            else:
-                matrix[i][-1] -= matrix[i][k]
-        vars[i] = round((matrix[i][-1] / matrix[i][i]) * 100) / 100
-    print(vars)
+    try:
+        vars = [1 for _ in range(len(matrix))]
+        for i in range(len(matrix) - 1, -1, -1):
+            for k in range(len(matrix[i]) - 1):
+                matrix[i][k] *= vars[k]
+            for k in range(len(matrix[i]) - 2, i, -1):
+                if i == len(matrix) - 1:
+                    break
+                else:
+                    matrix[i][-1] -= matrix[i][k]
+            vars[i] = round((matrix[i][-1] / matrix[i][i]) * 100) / 100
+        print(vars)
+    except ZeroDivisionError:
+        print("Проверьте введенную систему уравнений")
 
 
 n = int(input("Введите количество уравнений "))
@@ -61,11 +83,10 @@ for i in range(n):
     matrix_element = input("Введите коэффициенты уравнения (через пробел) ").split(' ')
     matrix_element_list = []
     for k in range(len(matrix_element)):
-        if matrix_element[k] ==' ':
+        if matrix_element[k] == ' ':
             continue
         else:
             matrix_element_list.append(float(matrix_element[k]))
     matrix_new.append(matrix_element_list)
-matrix_new = sorted(matrix_new, key=lambda matrix_new: abs(matrix_new[0]), reverse=True)
 gauss(matrix_new)
 
